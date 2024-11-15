9f8db1b5bcfcc54086ed1ec5ff22e44960adb76c
DeloranV <34947157+DeloranV@users.noreply.github.com>
2024-11-15 11:27:56 +0100

    added even more

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
-from PySide6.QtGui import Qt
-from PySide6.QtCore import Slot
+from PySide6.QtGui import Qt, QDoubleValidator
+from PySide6.QtCore import Slot, QLocale
 from PySide6.QtWidgets import QApplication, QDialog, QLabel, QPushButton, QRadioButton, QLineEdit, QVBoxLayout, QGridLayout, QHBoxLayout, QListWidget, QListWidgetItem
+from math import sqrt, log10, e
 #(TODO) WHEN AN ELEMENT HAS MORE THAN ONE BEHAVIOUR - CREATE A SEPERATE CLASS FOR IT FOR EXAMPLE RADIO BUTTONS WITH radio_checked, change_radio ETC...
+#(TODO) HOVER PIAST URL HINT
+class DragDropBttn(QPushButton):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.fileLoaded = False
+
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+
+    def dropEvent(self, event):
+        url = event.mimeData().urls()
+        local_url = url[0].toLocalFile()
+        print(local_url)
+
+        with open(local_url, 'r') as csv_file:
+            self.csv_file = csv_file.read()
+
+        self.fileLoaded = True
+
+class IntLineEdit(QLineEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def return_float(self):
+        return float(self.text())
+
 class MainCalc(QDialog):
+    LIGHT_SPEED = 299792458
+    EFFECTIVE_EARTH_CURVATURE = 1/6480
+
     def __init__(self, parent=None):
         super(MainCalc, self).__init__(parent) #Qt.WindowType.FramelessWindowHint FOR FRAMELESS WINDOWS
-
+        self.uploaded_distance = []
+        self.active_radio = None
+        self.POSITIVE_DOUBLE_VALIDATOR = QDoubleValidator(1, 1000000, 5)
+        self.POSITIVE_DOUBLE_VALIDATOR.setLocale(QLocale.Language.English)
         self.layout_init()
 
         self.setWindowTitle("Bullington Calculator")
@@ -41,14 +77,19 @@ class MainCalc(QDialog):
         save_button.setMaximumWidth(50)
         save_button.setText("Zapisz")
         save_button.clicked.connect(self.list_save)
+        save_button.setObjectName("save_button")
 
         remove_button = QPushButton()
         remove_button.setMaximumWidth(50)
         remove_button.setText("Usuń")
         remove_button.clicked.connect(self.list_remove)
+        remove_button.setObjectName("remove_button")
 
-        upload_button = QPushButton()
-        upload_button.setText("Z pliku CSV")
+        self.upload_button = DragDropBttn()
+        self.upload_button.setText("Upuść CSV")
+        self.upload_button.setObjectName("upload_button")
+        self.upload_button.setAcceptDrops(True)
+        self.upload_button.clicked.connect(self.drop_load)
 
         list_buttons_layout.addWidget(save_button)
         list_buttons_layout.addWidget(remove_button)
@@ -56,17 +97,45 @@ class MainCalc(QDialog):
         wrapper_layout.addWidget(self.routes_list)
         wrapper_layout.addWidget(self.name_line)
         wrapper_layout.addLayout(list_buttons_layout)
-        wrapper_layout.addWidget(upload_button)
+        wrapper_layout.addWidget(self.upload_button)
 
         return wrapper_layout
+
+    def drop_load(self):
+        csv_file = []
+        if self.upload_button.fileLoaded:
+            self.dropped = self.upload_button.csv_file.split("\n")
+
+            for row in self.dropped:
+                if "Index" not in row:
+                    csv_file.append(row.split(";"))
+
+            for row in range(len(csv_file)-1):
+                self.uploaded_distance.append([])
+                for column in 3,4:
+                    self.uploaded_distance[-1].append(float(csv_file[row][column].replace(',', '.')))
+
+            '''max_val = [0,0]
+            for row in self.uploaded_distance:
+                if row[1] > max_val[1]:
+                    max_val = row'''
+
+
+            self.uploaded_distance.sort(reverse=True, key=self.sort_key)
+
+            print(self.uploaded_distance)
+
+    def sort_key(self, e):  #for sorting list by second col
+        return e[1]
 
     def list_load(self) -> None:
         self.saved_data = []
 
         with open("saved.bull", "r") as data_file:
             for line in data_file:
-                self.saved_data.append(line.split(";"))
-        print(self.routes_list.findItems("test3", Qt.MatchFlag.MatchExactly))
+                if line[0] != "\n":
+                    self.saved_data.append(line.split(";"))
+
         for route in self.saved_data:
             self.routes_list.addItem(route[0])
 
@@ -153,7 +222,8 @@ class MainCalc(QDialog):
 
         between_title = QLabel("Dystans między antenami [km]")
         between_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.betweeen = QLineEdit()
+        self.betweeen = IntLineEdit()
+        self.betweeen.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
         self.betweeen.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.betweeen.setMaximumWidth(100)
 
@@ -167,12 +237,15 @@ class MainCalc(QDialog):
         txrx_label_layout.addSpacing(115)
         txrx_label_layout.addWidget(rx_h_title)
 
-        self.tx_h_line = QLineEdit()
+        self.tx_h_line = IntLineEdit()
         self.tx_h_line.setMaximumWidth(100)
         self.tx_h_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.rx_h_line = QLineEdit()
+        self.tx_h_line.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
+
+        self.rx_h_line = IntLineEdit()
         self.rx_h_line.setMaximumWidth(100)
         self.rx_h_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.rx_h_line.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
 
         txrx_height_layout.addWidget(self.tx_h_line)
         txrx_height_layout.addSpacing(180)
@@ -182,24 +255,28 @@ class MainCalc(QDialog):
         dist_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
         #(TODO) convert this d and h stuff to lists
         d1_label = QLabel("d1")
-        self.d1_line = QLineEdit()
+        self.d1_line = IntLineEdit()
         self.d1_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.d1_line.setMaximumWidth(100)
+        self.d1_line.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
 
         d2_label = QLabel("d2")
-        self.d2_line = QLineEdit()
+        self.d2_line = IntLineEdit()
         self.d2_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.d2_line.setMaximumWidth(100)
+        self.d2_line.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
 
         d3_label = QLabel("d3")
-        self.d3_line = QLineEdit()
+        self.d3_line = IntLineEdit()
         self.d3_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.d3_line.setMaximumWidth(100)
+        self.d3_line.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
 
         d4_label = QLabel("d4")
-        self.d4_line = QLineEdit()
+        self.d4_line = IntLineEdit()
         self.d4_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.d4_line.setMaximumWidth(100)
+        self.d4_line.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
 
         dist_layout.addWidget(d1_label)
         dist_layout.addWidget(self.d1_line)
@@ -221,24 +298,28 @@ class MainCalc(QDialog):
         height_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         h1_label = QLabel("h1")
-        self.h1_line = QLineEdit()
+        self.h1_line = IntLineEdit()
         self.h1_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.h1_line.setMaximumWidth(100)
+        self.h1_line.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
 
         h2_label = QLabel("h2")
-        self.h2_line = QLineEdit()
+        self.h2_line = IntLineEdit()
         self.h2_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.h2_line.setMaximumWidth(100)
+        self.h2_line.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
 
         h3_label = QLabel("h3")
-        self.h3_line = QLineEdit()
+        self.h3_line = IntLineEdit()
         self.h3_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.h3_line.setMaximumWidth(100)
+        self.h3_line.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
 
         h4_label = QLabel("h4")
-        self.h4_line = QLineEdit()
+        self.h4_line = IntLineEdit()
         self.h4_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.h4_line.setMaximumWidth(100)
+        self.h4_line.setValidator(self.POSITIVE_DOUBLE_VALIDATOR)
 
         height_layout.addWidget(h1_label)
         height_layout.addWidget(self.h1_line)
@@ -289,17 +370,19 @@ class MainCalc(QDialog):
         self.freq_radio_3.toggled.connect(self.radio_checked_3)
         self.freq_radio_4.toggled.connect(self.radio_checked_4)
 
-        freq_other = QLineEdit()
-        freq_other_label = QLabel("Inna")
-        freq_other.setMaximumWidth(50)
+        self.other_radio = QRadioButton(self)
+        self.freq_other = IntLineEdit()
+        self.freq_other.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.freq_other.setPlaceholderText("Inna")
+        self.freq_other.setMaximumWidth(50)
 
         radio_layout.addSpacing(50)
         radio_layout.addWidget(self.freq_radio_1)
         radio_layout.addWidget(self.freq_radio_2)
         radio_layout.addWidget(self.freq_radio_3)
         radio_layout.addWidget(self.freq_radio_4)
-        radio_layout.addWidget(freq_other)
-        radio_layout.addWidget(freq_other_label)
+        radio_layout.addWidget(self.other_radio)
+        radio_layout.addWidget(self.freq_other)
 
         label_layout.addWidget(freq_label)
         label_layout.addLayout(radio_layout)
@@ -337,6 +420,7 @@ class MainCalc(QDialog):
         if radio == 4:
             self.freq_radio_4.setChecked(True)
 
+
     def results_init(self) -> QVBoxLayout:
         results_layout = QVBoxLayout()
         button_box = QHBoxLayout()
@@ -345,6 +429,8 @@ class MainCalc(QDialog):
         srim_box = QHBoxLayout()
         str_box = QHBoxLayout()
         db_box = QHBoxLayout()
+        vb_box = QHBoxLayout()
+        luc_box = QHBoxLayout()
         loss_box = QHBoxLayout()
 
         results_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -354,6 +440,7 @@ class MainCalc(QDialog):
         calc_button = QPushButton()
         calc_button.setText("Oblicz")
         calc_button.setObjectName("calc_button")
+        calc_button.clicked.connect(self.results_calculate)
 
         button_box.addWidget(calc_button)
 
@@ -364,6 +451,8 @@ class MainCalc(QDialog):
         wavelength_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.wavelength_line = QLineEdit()
+        self.wavelength_line.setReadOnly(True)
+        self.wavelength_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         # wavelength_line.setMaximumWidth(100)
 
         wavelength_box.addSpacing(115)
@@ -374,6 +463,8 @@ class MainCalc(QDialog):
 
         stim_title = QLabel("Horyzont radiowy nadajnika [m/km]")
         self.stim_line = QLineEdit()
+        self.stim_line.setReadOnly(True)
+        self.stim_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         stim_box.addSpacing(50)
         stim_box.addWidget(stim_title)
         stim_box.addSpacing(118)
@@ -382,6 +473,8 @@ class MainCalc(QDialog):
 
         srim_title = QLabel("Horyzont radiowy odbiornika [m/km]")
         self.srim_line = QLineEdit()
+        self.srim_line.setReadOnly(True)
+        self.srim_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         srim_box.addSpacing(50)
         srim_box.addWidget(srim_title)
         srim_box.addSpacing(113)
@@ -390,6 +483,8 @@ class MainCalc(QDialog):
 
         str_title = QLabel("Linia bezpośredniej widoczności [m/km]")
         self.str_line = QLineEdit()
+        self.str_line.setReadOnly(True)
+        self.str_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         str_box.addSpacing(50)
         str_box.addWidget(str_title)
         str_box.addSpacing(99)
@@ -398,14 +493,38 @@ class MainCalc(QDialog):
 
         db_title = QLabel("Odległość punktu Bullingtona od Tx [km]")
         self.db_line = QLineEdit()
+        self.db_line.setReadOnly(True)
+        self.db_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         db_box.addSpacing(50)
         db_box.addWidget(db_title)
         db_box.addSpacing(95)
         db_box.addWidget(self.db_line)
         db_box.addSpacing(85)
 
+        vb_title = QLabel("Parametr v Fresnela-Kirchoffa")
+        self.vb_line = QLineEdit()
+        self.vb_line.setReadOnly(True)
+        self.vb_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        vb_box.addSpacing(80)
+        vb_box.addWidget(vb_title)
+        vb_box.addSpacing(127)
+        vb_box.addWidget(self.vb_line)
+        vb_box.addSpacing(85)
+
+        luc_title = QLabel("Straty dyfrakcyjne wirtualnej przeszkody [dB]")
+        self.luc_line = QLineEdit()
+        self.luc_line.setReadOnly(True)
+        self.luc_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        luc_box.addSpacing(50)
+        luc_box.addWidget(luc_title)
+        luc_box.addSpacing(78)
+        luc_box.addWidget(self.luc_line)
+        luc_box.addSpacing(85)
+
         loss_title = QLabel("Łączne straty dyfrakcyjne [dB]")
         self.loss_line = QLineEdit()
+        self.loss_line.setReadOnly(True)
+        self.loss_line.setAlignment(Qt.AlignmentFlag.AlignCenter)
         loss_box.addSpacing(85)
         loss_box.addWidget(loss_title)
         loss_box.addSpacing(120)
@@ -424,6 +543,10 @@ class MainCalc(QDialog):
         results_layout.addSpacing(15)
         results_layout.addLayout(db_box)
         results_layout.addSpacing(15)
+        results_layout.addLayout(vb_box)
+        results_layout.addSpacing(15)
+        results_layout.addLayout(luc_box)
+        results_layout.addSpacing(15)
         results_layout.addLayout(loss_box)
         results_layout.addSpacing(15)
 
@@ -437,6 +560,111 @@ class MainCalc(QDialog):
         self.right_side_layout = QVBoxLayout(self)
 
 
+    #def check_non_empty(self):
+       #if (self.betweeen.text() != "" and self.tx_h_line.text() != "" and self.rx_h_line.text() != ""
+            #and self.d1_line.text() != "" and self.d2_line.text() != "" and self.d3_line.text() != ""
+            #and self.d4_line.text() != "" and self.h1_line.text() != "" and self.h2_line.text() != ""
+            #and self.h3_line.text() != "" and self.h4_line.text() != "" and self.active_radio != None):
+            #return True    ALTERNATIVE TO TRY CATCH LOL
+
+    def results_calculate(self):
+        #if self.check_non_empty():
+        if self.active_radio == 1:
+            f = 174 * pow(10,6)
+        elif self.active_radio == 2:
+            f = 880 * pow(10,6)
+        elif self.active_radio == 3:
+            f = 2500 * pow(10,6)
+        elif self.active_radio == 4:
+            f = 3500 * pow(10,6)
+
+        try:
+
+            self.wavelength = self.LIGHT_SPEED/f
+            self.wavelength_line.setText(str(round(self.wavelength, 3)))
+
+            s_tim1 = ((self.h1_line.return_float() +
+                       500 * self.EFFECTIVE_EARTH_CURVATURE * self.d1_line.return_float() * (self.betweeen.return_float() - self.d1_line.return_float()) - self.tx_h_line.return_float())
+                      / self.d1_line.return_float())
+
+            s_tim2 = ((self.h2_line.return_float() +
+                       500 * self.EFFECTIVE_EARTH_CURVATURE * self.d2_line.return_float() * (
+                               self.betweeen.return_float() - self.d2_line.return_float()) - self.tx_h_line.return_float())
+                      / self.d2_line.return_float())
+
+            s_tim3 = ((self.h3_line.return_float() +
+                       500 * self.EFFECTIVE_EARTH_CURVATURE * self.d3_line.return_float() * (
+                               self.betweeen.return_float() - self.d3_line.return_float()) - self.tx_h_line.return_float())
+                      / self.d3_line.return_float())
+
+            s_tim4 = ((self.h4_line.return_float() +
+                       500 * self.EFFECTIVE_EARTH_CURVATURE * self.d4_line.return_float() * (
+                               self.betweeen.return_float() - self.d4_line.return_float()) - self.tx_h_line.return_float())
+                      / self.d4_line.return_float())
+
+            self.stim = round(max(s_tim1,s_tim2,s_tim3,s_tim4),3)
+
+            self.stim_line.setText(str(self.stim))
+
+            s_tr = round((self.rx_h_line.return_float() - self.tx_h_line.return_float()) / self.betweeen.return_float(), 3)
+            self.str_line.setText(str(s_tr))
+
+            rx_d1 = self.betweeen.return_float() - self.d1_line.return_float()
+            rx_d2 = self.betweeen.return_float() - self.d2_line.return_float()
+            rx_d3 = self.betweeen.return_float() - self.d3_line.return_float()
+            rx_d4 = self.betweeen.return_float() - self.d4_line.return_float()
+
+            s_rim1 = ((self.h1_line.return_float() +
+                       500 * self.EFFECTIVE_EARTH_CURVATURE * rx_d1 *
+                       (self.betweeen.return_float() - rx_d1) - self.rx_h_line.return_float())
+                      / (self.betweeen.return_float() - rx_d1))
+
+            s_rim2 = ((self.h2_line.return_float() +
+                       500 * self.EFFECTIVE_EARTH_CURVATURE * rx_d2 *
+                       (self.betweeen.return_float() - rx_d2) - self.rx_h_line.return_float())
+                      / (self.betweeen.return_float() - rx_d2))
+
+            s_rim3 = ((self.h3_line.return_float() +
+                       500 * self.EFFECTIVE_EARTH_CURVATURE * rx_d3 *
+                       (self.betweeen.return_float() - rx_d3) - self.rx_h_line.return_float())
+                      / (self.betweeen.return_float() - rx_d3))
+
+            s_rim4 = ((self.h4_line.return_float() +
+                       500 * self.EFFECTIVE_EARTH_CURVATURE * rx_d4 *
+                       (self.betweeen.return_float() - rx_d4) - self.rx_h_line.return_float())
+                      / (self.betweeen.return_float() - rx_d4))
+
+            self.srim = round(max(s_rim1, s_rim2, s_rim3, s_rim4), 3)
+
+            self.srim_line.setText(str(self.srim))
+
+            self.db = ((self.rx_h_line.return_float() - self.tx_h_line.return_float() + self.srim * self.betweeen.return_float())
+                  / (self.stim+self.srim))
+
+            self.db_line.setText(str(round(self.db,3)))
+
+            self.vb = (self.tx_h_line.return_float() + self.stim * self.db -
+                       (self.tx_h_line.return_float() * (self.betweeen.return_float() - self.db) + self.rx_h_line.return_float() * self.db)
+                       / self.betweeen.return_float()) * sqrt((0.002 * self.betweeen.return_float()) / (self.wavelength * self.db * (self.betweeen.return_float() - self.db)))
+
+            self.vb_line.setText(str(round(self.vb,3)))
+
+            self.luc = 6.9 + 20*log10(sqrt(pow(self.vb-0.1,2)+1)+self.vb-0.1)
+
+            self.luc_line.setText(str(round(self.luc,3)))
+
+            self.lb = self.luc+(1-pow(e,-self.luc/6))*(10 + 0.02 * self.betweeen.return_float())
+
+            self.loss_line.setText(str(round(self.lb, 3)))
+
+        except UnboundLocalError:
+            print("Zaznacz częstotliwość")
+
+        except ValueError:
+            print("Wypełnij wszystkie pola")
+
+        except ZeroDivisionError:
+            print("Błąd dzielenia przez zero - dobierz inne wartości")
 
 if __name__ == '__main__':
     app = QApplication()
